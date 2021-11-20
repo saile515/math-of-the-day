b8fce74e239153797ce31deb028ec32c2835fab3
saile515 <elias.jorgensen2006@gmail.com>
2021-11-20 12:36:45 +0100

    Added title

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,12 +5,18 @@ import styles from "../styles/home.module.scss";
 
 const Home: NextPage = () => {
 	return (
-		<div className={styles.difficultyContainer}>
-			<DifficultyButton difficulty={Difficulty.Easy} />
-			<DifficultyButton difficulty={Difficulty.Medium} />
-			<DifficultyButton difficulty={Difficulty.Hard} />
-			<DifficultyButton difficulty={Difficulty.Expert} />
-			<DifficultyButton difficulty={Difficulty.Impossible} />
+		<div className={styles.pageContainer}>
+			<div className={styles.header}>
+				<h1 className={styles.title}>Math of the Day</h1>
+				<h2 className={styles.subheading}>Select difficulty</h2>
+			</div>
+			<div className={styles.difficultyContainer}>
+				<DifficultyButton difficulty={Difficulty.Easy} />
+				<DifficultyButton difficulty={Difficulty.Medium} />
+				<DifficultyButton difficulty={Difficulty.Hard} />
+				<DifficultyButton difficulty={Difficulty.Expert} />
+				<DifficultyButton difficulty={Difficulty.Impossible} />
+			</div>
 		</div>
 	);
 };
